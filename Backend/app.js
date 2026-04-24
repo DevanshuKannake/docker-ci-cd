2b0b7e84ac1117c9231f7d23de54ac36ec0dc17e
DevanshuKannake <devanshukannake480@gmail.com>
2026-04-25 02:05:02 +0530

    Initial commit with CI/CD

---
+++ Backend/app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send("Backend Service Running 🚀");
+});
+
+app.listen(5000, () => {
+  console.log("Backend running on port 5000");
+});
